15f9fb198f15c4603bd1c6edaa1d136b0a0e5857
Artem <55680818+konick24@users.noreply.github.com>
2025-05-01 23:45:28 +0300

    Task 4 (#4)
    * Сделана версия для планшетов
    
    * Добавлена графика на десктопную версию

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
+const WIDTH_MEDIUM = 768;
+
 const nav = document.querySelector('.nav');
 const navButton = nav.querySelector('.nav__button');
 const navList = nav.querySelector('.nav__list');
+const page = document.querySelector(".page");
 
 navButton.addEventListener('click', (evt) => {
   evt.preventDefault();
@@ -8,3 +11,11 @@ navButton.addEventListener('click', (evt) => {
   navButton.classList.toggle('nav__button--closed');
   navList.classList.toggle('nav__list--opened');
 });
+
+const onWindowScroll = () => {
+  if (window.screen.width >= WIDTH_MEDIUM) {
+    document.querySelector('.page-header').classList.toggle('page-header--scrolled', window.scrollY > 88);
+  }
+}
+
+window.addEventListener("scroll", onWindowScroll);
